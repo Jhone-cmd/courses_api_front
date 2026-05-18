f362bd2c5e0ba291c9fc883cd69c45de9b7d6819
Jhone-cmd <jhone.1941@gmail.com>
2026-05-18 16:00:30 -0300

    add spring security.

---
+++ src/main/java/br/com/jhonecmd/courses_api_front/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.jhonecmd.courses_api_front.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/users/login")
+                    .permitAll();
+            auth.anyRequest().authenticated();
+        }).formLogin(form -> form.loginPage("/users/login"));
+        return http.build();
+    }
+}
