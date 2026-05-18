ba17447be9be42cd5e12ee21bde5a8ae95ca9172
Jhone-cmd <jhone.1941@gmail.com>
2026-05-18 15:18:16 -0300

    initial commit and setup.

---
+++ src/main/java/br/com/jhonecmd/courses_api_front/CoursesApiFrontApplication.java
@@ -0,0 +1,13 @@
+package br.com.jhonecmd.courses_api_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApiFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApiFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jhonecmd/courses_api_front/CoursesApiFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhonecmd.courses_api_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApiFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
